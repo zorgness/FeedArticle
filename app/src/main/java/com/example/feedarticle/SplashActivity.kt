c6caf4a24a98416d4d5839c8debc798937bb5909
Thomas Fouquert <tfouquert@gmail.com>
2023-04-14 13:51:07 +0200

    first commit

---
+++ app/src/main/java/com/example/feedarticle/SplashActivity.kt
@@ -0,0 +1,38 @@
+package com.example.feedarticle
+
+import SHAREDPREF_LOGIN_TOKEN
+import SHAREDPREF_NAME
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+
+            applicationContext.getSharedPreferences(SHAREDPREF_NAME, Context.MODE_PRIVATE).apply {
+
+                (
+                        getString(SHAREDPREF_LOGIN_TOKEN, null)
+                            ?.run {
+                                MainActivity::class.java
+                            }
+                            ?: LoginActivity::class.java
+                        )
+                    .let {c->
+                        startActivity(Intent(this@SplashActivity, c))
+                        finish()
+                    }
+
+            }
+
+        }, 2000)
+
+    }
+}
---
+++ app/src/main/java/com/example/feedarticle/constant/Constant.kt
@@ -0,0 +1,2 @@
+const val SHAREDPREF_NAME = "feedarticle"
+const val SHAREDPREF_LOGIN_TOKEN = "login token"
