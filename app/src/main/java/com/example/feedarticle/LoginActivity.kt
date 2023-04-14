445e4d68ecea032a7c62aa93d92697e714afe07f
Thomas Fouquert <tfouquert@gmail.com>
2023-04-14 16:27:10 +0200

    update on register activity

--- app/src/main/java/com/example/feedarticle/LoginActivity.kt
+++ app/src/main/java/com/example/feedarticle/LoginActivity.kt
@@ -1,11 +1,19 @@
 package com.example.feedarticle
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+
+        findViewById<TextView>(R.id.tv_register_login_activity).setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+            finish()
+        }
     }
 }
