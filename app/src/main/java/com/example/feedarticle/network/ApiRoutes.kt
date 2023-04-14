0826efeb337291acea554312aefb7eaa6b125344
Thomas Fouquert <tfouquert@gmail.com>
2023-04-14 16:34:09 +0200

    add api routes

---
+++ app/src/main/java/com/example/feedarticle/network/ApiRoutes.kt
@@ -0,0 +1,14 @@
+package com.example.feedarticle.network
+
+object ApiRoutes {
+
+    const val BASE_URL = "https://dev.dev-id.fr/formation/articles/"
+    const val REGISTER = "register.php"
+    const val LOGIN = "login.php"
+    const val ARTICLES  = "articles.php"
+    const val ARTICLE = "article.php"
+    const val NEW = "new.php"
+    const val UPDATE = "update.php"
+    const val DELETE = "delete.php"
+
+}
