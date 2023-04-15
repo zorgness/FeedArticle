86bf4ef1a1fe2274611f2ec33bd3908cdfeb97f9
Thomas Fouquert <tfouquert@gmail.com>
2023-04-15 08:32:42 +0200

    init on create edit article activity

--- app/src/main/java/com/example/feedarticle/CreateOrEditActivity.kt
+++ app/src/main/java/com/example/feedarticle/CreateOrEditActivity.kt
@@ -2,10 +2,26 @@ package com.example.feedarticle
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
 
 class CreateOrEditActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_or_edit)
+
+        val etUrlImg = findViewById<EditText>(R.id.et_article_url_img)
+        val etTitle = findViewById<EditText>(R.id.et_article_title)
+        val etDescription = findViewById<EditText>(R.id.et_article_description)
+        val spinnerCategory = findViewById<Spinner>(R.id.spinner_category_form)
+
+
+        findViewById<Button>(R.id.btn_new_edit_article).setOnClickListener {
+            val urlImg = etUrlImg.text.toString()
+            val title = etTitle.text.toString()
+            val description = etDescription.text.toString()
+
+        }
     }
 }
