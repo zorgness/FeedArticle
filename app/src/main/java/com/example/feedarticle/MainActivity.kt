d2a874dcc088636ca3d8d2a19f5034984fd88b99
Thomas Fouquert <tfouquert@gmail.com>
2023-04-15 08:17:48 +0200

    update on register activity

--- app/src/main/java/com/example/feedarticle/MainActivity.kt
+++ app/src/main/java/com/example/feedarticle/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.feedarticle
 
+import SHAREDPREF_LOGIN_TOKEN
+import SHAREDPREF_NAME
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        with(applicationContext.getSharedPreferences(SHAREDPREF_NAME, Context.MODE_PRIVATE)) {
+            findViewById<Button>(R.id.btn_logout).setOnClickListener {
+                edit().remove(SHAREDPREF_LOGIN_TOKEN).apply()
+                startActivity(Intent(this@MainActivity, LoginActivity::class.java))
+                finish()
+            }
+        }
+
     }
 }
--- app/src/main/java/com/example/feedarticle/RegisterActivity.kt
+++ app/src/main/java/com/example/feedarticle/RegisterActivity.kt
@@ -1,11 +1,54 @@
 package com.example.feedarticle
 
+import SHAREDPREF_LOGIN_TOKEN
+import SHAREDPREF_NAME
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import register
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        val etLogin = findViewById<EditText>(R.id.et_user_login)
+        val etPassword = findViewById<EditText>(R.id.et_user_password)
+        val etPwdConfirm = findViewById<EditText>(R.id.et_user_password_confirm)
+
+
+
+
+        findViewById<Button>(R.id.btn_submit_register).setOnClickListener {
+            val login = etLogin.text.toString()
+            val password = etPassword.text.toString()
+            val confirm = etPwdConfirm.text.toString()
+
+            if(login.isNotBlank() && password.isNotBlank()) {
+                if(validatePassword(password, confirm)) {
+                    register(login, password, loginCallback = {
+                        with(it.token) {
+                            applicationContext.getSharedPreferences(SHAREDPREF_NAME, Context.MODE_PRIVATE)
+                                .edit()
+                                .putString(SHAREDPREF_LOGIN_TOKEN, this)
+                                .apply()
+                            startActivity(Intent(this@RegisterActivity, MainActivity::class.java))
+                            finish()
+                        }
+                    })
+
+                } else {
+                    //error message password and confirm not the same
+                }
+            } else {
+                //error message field is empty
+            }
+        }
+    }
+    private fun validatePassword(password: String, confirm: String): Boolean {
+        return password == confirm
     }
 }
