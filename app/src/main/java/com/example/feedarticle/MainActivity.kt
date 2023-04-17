0c27e0c59abcf32b71e936293b1ecfa695e158e3
Thomas Fouquert <tfouquert@gmail.com>
2023-04-17 09:22:51 +0200

    add filter on category

--- app/src/main/java/com/example/feedarticle/MainActivity.kt
+++ app/src/main/java/com/example/feedarticle/MainActivity.kt
@@ -6,7 +6,11 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.Spinner
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.feedarticle.dataclass.SessionDto
@@ -15,14 +19,17 @@ import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import getRemoteArticles
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
 
     companion object {
         const val KEY_ARTICLE_ID = "key article id"
     }
 
+    var callBack: ((Int) -> Unit)? = null
     private lateinit var recyclerView: RecyclerView
     private lateinit var articleAdapter: ArticleAdapter
+    lateinit var spinnerCategory: Spinner
+    var session: SessionDto? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -32,6 +39,10 @@ class MainActivity : AppCompatActivity() {
         articleAdapter = ArticleAdapter()
         recyclerView.adapter = articleAdapter
 
+        spinnerCategory = findViewById<Spinner>(R.id.spinner_category)
+
+
+
 
         var session: SessionDto? = convertJsonToDto(
             applicationContext
@@ -40,11 +51,39 @@ class MainActivity : AppCompatActivity() {
 
         )
 
+
         fun refresh() {
             if (session != null) {
                 getRemoteArticles(session.token){
-                    articleAdapter.setArticles(it)
+                    if(spinnerCategory.selectedItemId.toInt() > 0) {
+                        articleAdapter.setArticles(it.filter { el -> el.categorie == spinnerCategory.selectedItemId.toInt() })
+                    } else
+                        articleAdapter.setArticles(it)
                 }
+            }
+        }
+
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.categories,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            spinnerCategory.adapter = adapter
+            spinnerCategory.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                    //refresh()
+                    println("spinnerCategory")
+                    refresh()
+                }
+
+                override fun onNothingSelected(p0: AdapterView<*>?) {
+                    TODO("Not yet implemented")
+                }
+
             }
         }
 
@@ -83,5 +122,18 @@ class MainActivity : AppCompatActivity() {
             Moshi.Builder().addLast(KotlinJsonAdapterFactory())
                 .build().adapter(SessionDto::class.java).fromJson(it)
         }
+    }
+
+
+
+    override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+        println("onItemSelected $p2")
+        //refresh()
+
+
+    }
+
+    override fun onNothingSelected(p0: AdapterView<*>?) {
+        //TODO("Not yet implemented")
     }
 }
